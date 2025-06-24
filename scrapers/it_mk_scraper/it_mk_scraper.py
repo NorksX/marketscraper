4fcd4bf64e7bf6ac7180df222d17aa725c4b1b60
NorksX <borismanev03@gmail.com>
2025-06-25 00:10:31 +0200

    Testing Jenkins CI (Test 4)

--- scrapers/it_mk_scraper/it_mk_scraper.py
+++ scrapers/it_mk_scraper/it_mk_scraper.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup, NavigableString
 import time
 from ad import Ad
 
-#Comment for testing Jenkins CI test 3
+#Comment for testing Jenkins CI test 4
 
 # === COLOR CONSTANTS ===
 RED = '\033[31m'
